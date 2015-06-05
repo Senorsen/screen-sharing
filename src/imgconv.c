ccb9d71faa789662b27007febc4be1acd3e25d7b
Senorsen <sen@senorsen.com>
2015-06-05 22:55:06 +0800

    Fixed rgba2rgb and rgb2yuv

--- src/imgconv.c
+++ src/imgconv.c
@@ -1,9 +1,9 @@
 
 #include "imgconv.h"
 
-#define sR (unsigned char)(rgb[s+2])
-#define sG (unsigned char)(rgb[s+1])
-#define sB (unsigned char)(rgb[s+0])
+#define sR (unsigned char)(rgb[s])
+#define sG (unsigned char)(rgb[s + 1])
+#define sB (unsigned char)(rgb[s + 2])
 
 void rgb2yuv420p(unsigned int width, unsigned int height, unsigned char *rgb, unsigned char *yuv420p) {
     unsigned int i = 0;
@@ -14,7 +14,7 @@ void rgb2yuv420p(unsigned int width, unsigned int height, unsigned char *rgb, un
 
     for (int j = 0; j < height; j++) {
         for (int k = 0; k < width; k++) {
-            s = j * width * 3 + i;
+            s = j * width * 3 + k * 3;
             yuv420p[i] = (unsigned char)((66*sR + 129*sG + 25*sB + 128) >> 8) + 16;
 
             if (0 == j % 2 && 0 == k % 2) {
@@ -31,9 +31,9 @@ void rgba2rgb(unsigned int width, unsigned int height, unsigned char *rgba, unsi
     unsigned int j = 0;
     for (i = 0; i < width; i++) {
         for (j = 0; j < height; j++) {
-            rgb[j * width * 3 + i * 3] = rgba[j * width * 4 + i * 4];
+            rgb[j * width * 3 + i * 3] = rgba[j * width * 4 + i * 4 + 2];
             rgb[j * width * 3 + i * 3 + 1] = rgba[j * width * 4 + i * 4 + 1];
-            rgb[j * width * 3 + i * 3 + 2] = rgba[j * width * 4 + i * 4 + 2];
+            rgb[j * width * 3 + i * 3 + 2] = rgba[j * width * 4 + i * 4];
         }
     }
 }
--- src/main.c
+++ src/main.c
@@ -13,13 +13,39 @@
 #include "pointer.h"
 #include "imgconv.h"
 
-int generate_timestamp() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec;
+void output_jpeg(unsigned int width, unsigned int height, unsigned char *rgbdata, char *filename, int quality) {
+    FILE *fp = fopen(filename, "wb");
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+
+    jpeg_stdio_dest(&cinfo, fp);
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_RGB;
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, quality, 1);
+    jpeg_start_compress(&cinfo, 1);
+
+    JSAMPROW row_pointer[1];
+    unsigned char *pBuf = (unsigned char *) malloc(3 * width);
+    row_pointer[0] = pBuf;
+    while (cinfo.next_scanline < cinfo.image_height) {
+        memcpy(pBuf, rgbdata + 3 * cinfo.next_scanline * width, 3 * width);
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+    free(pBuf);
+    jpeg_finish_compress(&cinfo);
+    jpeg_destroy_compress(&cinfo);
+    fclose(fp);
 }
 
-int capture_desktop() {
+
+int capture_desktop(unsigned int *width, unsigned int *height, unsigned char *yuvdata) {
     Window desktop;
     Display *disp;
     XImage *img;
@@ -40,16 +66,14 @@ int capture_desktop() {
     screen_width = SCREEN_WIDTH;
     screen_height = SCREEN_HEIGHT;
     img = XGetImage(disp, desktop, SCREEN_LEFT, SCREEN_TOP, screen_width, screen_height, AllPlanes, ZPixmap);
-    
+
     unsigned int size = sizeof(unsigned char) * img->width * img->height * 1.5 * 3;
     unsigned char *rgbdata = (unsigned char *) malloc(size);
-    unsigned char *yuvdata = (unsigned char *) malloc(size);
+    yuvdata = (unsigned char *) malloc(size);
 
     rgba2rgb(img->width, img->height, img->data, rgbdata);
     rgb2yuv420p(img->width, img->height, rgbdata, yuvdata);
-    FILE *fp = fopen("test.test", "wb");
-    fwrite(yuvdata, size, 1, fp);
-    fclose(fp);
+    free(rgbdata);
 
     XDestroyImage(img);
     XCloseDisplay(disp);
@@ -57,7 +81,9 @@ int capture_desktop() {
 }
 
 int main(int argc, char *argv[]) {
-    capture_desktop();
+    unsigned int width, height;
+    unsigned char *yuvdata;
+    capture_desktop(&width, &height, yuvdata);
     return 0;
 }
 
